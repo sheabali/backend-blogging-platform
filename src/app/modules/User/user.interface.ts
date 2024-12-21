9b89f2958e3a2fa69914db429127c4b94394f3e2
sheabali <iamskpranto@gmail.com>
2024-12-22 04:06:17 +0600

    user model basic need completed

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,7 +1,21 @@
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
 export interface TUser {
-  name: string;
+  name: UserName;
   email: string;
   password: string;
   role: string;
   isBlocked: boolean;
 }
+
+// export interface UserModel extends Model<TUser> {
+//   isUserExistsByCustomId(id: string);
+//   isPasswordMatched(
+//     plainTextPassword: string,
+//     hashedPassword: string,
+//   ): Promise<boolean>;
+// }
---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from 'mongoose';
+import { TUser, UserName } from './user.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new Schema<TUser>({
+  name: userNameSchema,
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: 0,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const User = model<TUser>('User', userSchema);
+
+// name: string – The full name of the user.
+// email: string – The email address of the user, used for authentication and communication.
+// password: string – The password for the user, securely stored.
+// role: "admin" | "user" – The role of the user, determining their access level. Default is "user".
+// isBlocked: boolean – A flag indicating whether the user is blocked or not. Default is false.
+// createdAt: Date – The timestamp when the user was created.
+// updatedAt: Date – The timestamp of the last update to the user.
