58b62f2cd8b0b5622ba55b03ca04bc6df36ec39b
sheabali <iamskpranto@gmail.com>
2024-12-22 05:50:25 +0600

    change somethig

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(cookieParser());
 app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // application routes
-app.use('/api/auth/', router);
+app.use('/api/', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello developers!!!');
