6f37f87f3adc2a9088bb6fd487758e4c148c5279
sheabali <iamskpranto@gmail.com>
2024-12-22 04:29:18 +0600

    create all necessary file in blog folder

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -43,11 +43,3 @@ const userSchema = new Schema<TUser>(
 );
 
 export const User = model<TUser>('User', userSchema);
-
-// name: string – The full name of the user.
-// email: string – The email address of the user, used for authentication and communication.
-// password: string – The password for the user, securely stored.
-// role: "admin" | "user" – The role of the user, determining their access level. Default is "user".
-// isBlocked: boolean – A flag indicating whether the user is blocked or not. Default is false.
-// createdAt: Date – The timestamp when the user was created.
-// updatedAt: Date – The timestamp of the last update to the user.
