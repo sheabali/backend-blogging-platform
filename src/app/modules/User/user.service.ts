1b2769b189f66aa6e3ae4fc27b6c6274e5941ca1
sheabali <iamskpranto@gmail.com>
2024-12-23 01:29:51 +0600

    all ok code properly working

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,7 +1,10 @@
 import { TUser } from './user.interface';
+import { User } from './user.model';
 
 const createUserIntoDB = async (payload: TUser) => {
-  console.log(payload);
+  const result = await User.create(payload);
+
+  return result;
 };
 
 export const UserServices = {
