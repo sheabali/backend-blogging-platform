3d9d6e5691c59ee17f23b2a6c2f0ac319cd494e0
sheabali <iamskpranto@gmail.com>
2024-12-30 02:08:13 +0600

    done not found

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/global.ErrorHandler';
+import notFound from './app/middlewares/notFound';
 import { router } from './app/routes';
 
 const app: Application = express();
@@ -14,19 +15,19 @@ app.use(cors({ origin: ['http://localhost:5173'] }));
 // application routes
 app.use('/api', router);
 
-// const test = async (req: Request, res: Response) => {
-//   Promise.reject();
-//   //  const a = 10;
-//   // res.send(a);
-// };
+const test = async (req: Request, res: Response) => {
+  res.send(
+    `<div style="background: linear-gradient(135deg, #6a11cb, #2575fc); border-radius: 20px; width: 600px; height: 250px; margin: auto; margin-top: 50px; box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2); display: flex; flex-direction: column; justify-content: center; align-items: center;">
+       <h1 style="color: white; font-family: Arial, sans-serif; font-size: 24px; margin: 0; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);">Welcome to the Blog Server!</h1>
+     
+     </div>`,
+  );
+};
 
-// app.use('/', test);
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+app.get('/', test);
 
 // Error-handling middleware
 app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
--- src/app/middlewares/notFound.ts
+++ src/app/middlewares/notFound.ts
@@ -1,13 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(StatusCodes.NOT_FOUND).json({
+  res.status(StatusCodes.NOT_FOUND).json({
     success: false,
-    message: 'API Not Found !!',
+    message: 'Api not found',
     error: '',
   });
 };
