e5e84b86e42fe8ef06b9abeab26758b3957c1787
sheabali <iamskpranto@gmail.com>
2024-12-30 02:25:12 +0600

    done blog work

--- src/app/modules/Blog/blog.interface.ts
+++ src/app/modules/Blog/blog.interface.ts
@@ -6,12 +6,3 @@ export type TBlog = {
   author: Types.ObjectId;
   isPublished: boolean;
 };
-
-// Blog Model:
-
-// title: string – The title of the blog post.
-// content: string – The main body or content of the blog post.
-// author: ObjectId – A reference to the User model, indicating the author of the blog post.
-// isPublished: boolean – A flag indicating whether the blog post is published. Default is true (published).
-// createdAt: Date – The timestamp when the blog post was created.
-// updatedAt: Date – The timestamp of the last update to the blog post.
